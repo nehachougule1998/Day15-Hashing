cd51f28265d5f24f719aaa306fd0573ad312fabb
nehachougule1998 <nchougule734@gmail.com>
2022-03-27 11:18:46 +0530

    UC3 added

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,29 +1,27 @@
 package com.bridgelabz;
 
-import java.util.ArrayList;
-
 public class HashTableMain {
-public static void main(String[] args) {
 
-    MyHashTable<String, Integer> hashTable = new MyHashTable();
-    String message = "Paranoids are not paranoid because they are paranoid but " +
-            "because they keep putting themselves deliberately into paranoid avoidable situations";
+	public static void main(String[] args) {
 
-    String[] messageArray = message.toLowerCase().split(" ");
+          MyHashTable<String, Integer> hashTable = new MyHashTable();
+	      String message = "Paranoids are not paranoid because they are paranoid but " +
+	            "because they keep putting themselves deliberately into paranoid avoidable situations";
+	      
+	      String[] messageArray = message.toLowerCase().split(" ");
 
-
-    for (String word : messageArray) {
-        Integer value = hashTable.get(word);
-
-
-        if( value == null)
-            value = 1;
-        else
-
-            value = value + 1;
-        hashTable.add(word , value);
-    }
-    System.out.println(hashTable);
+	        for (String word : messageArray) {
+	            Integer value = hashTable.get(word);
+	            
+	            if( value == null)
+	                value = 1;
+	            else
+	            	value = value + 1;
+	            
+	            hashTable.add(word , value);
+	        }
+	        System.out.println(hashTable);
+	        hashTable.remove("avoidable");
+	        System.out.println(hashTable);
+	}	
 }
-}
-
