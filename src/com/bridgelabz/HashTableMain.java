4ff8f2afd98cbdcc267eaf6bdee9539919e080af
nehachougule1998 <nchougule734@gmail.com>
2022-03-27 10:51:44 +0530

    UC2 added

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,25 +1,29 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class HashTableMain {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
-        MyHashTable<String, Integer> hashTable = new MyHashTable();
-        String sentence = "To be or not to be";
+    MyHashTable<String, Integer> hashTable = new MyHashTable();
+    String message = "Paranoids are not paranoid because they are paranoid but " +
+            "because they keep putting themselves deliberately into paranoid avoidable situations";
 
-
-        String[] sentenceArray = sentence.toLowerCase().split(" ");
-
-        for (String word : sentenceArray) {
-            Integer value = hashTable.get(word);
+    String[] messageArray = message.toLowerCase().split(" ");
 
 
-            if( value == null)
-                value = 1;
-            else
-                value = value + 1;
-            hashTable.add(word , value);
-        }
-        System.out.println(hashTable);
+    for (String word : messageArray) {
+        Integer value = hashTable.get(word);
+
+
+        if( value == null)
+            value = 1;
+        else
+
+            value = value + 1;
+        hashTable.add(word , value);
     }
+    System.out.println(hashTable);
+}
 }
 
